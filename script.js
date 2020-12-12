66d8c27c70925233a0ec599534b0594ff6e1ea6e
anjelicatizon <anjelicatizon@gmail.com>
2020-12-12 12:02:43 -0500

    initial commit with pseudo code

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('HELLO')
+
+// PSUEDO CODE
+    // Grab value/info that the user entered in the form/input
+    // Create a function that encodes
+        // IDEAS:
+        // Create an object that defines the alphabet
+        // use split() to break the string up into individual characters (need to make sure order is maintained)
+        // Look up the index/position that the original letter is at (maybe using indexOf())
+        // function will take the index and add 5 to it and return the letter at that index (index + 5)
+        // loop through each character/letter in the string and append to the page 
+            // Maybe use .map since it returns a new array and we can store it in a variable and append that variable to the page
+    // Need to make sure that the function goes back to the beginning of the alphabet if it hits the last index number
+        // how?
+    // Resources: https://medium.com/javascript-in-plain-english/how-to-build-a-simple-cipher-machine-with-vanilla-javascript-62d401d4841
